d89ecd925b2539c7d251d3defb70345aedb5c35d
KimInteger <kjs20151240@gmail.com>
2024-06-18 15:22:09 +0900

    data insert

--- databaseM/INSERT.js
+++ databaseM/INSERT.js
@@ -1,1 +1,10 @@
-console.log('INSERT');
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('../database/info.db');
+
+db.run("INSERT INTO info (user, age) VALUES(?,?)",["Integer",29],(err)=>{
+  if(err){
+    console.log(err.message);
+  }
+  console.log('data insert');
+});
