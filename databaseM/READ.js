23d847d24b315d55121097fbd82a23a3d3e0da19
KimInteger <kjs20151240@gmail.com>
2024-06-18 15:34:41 +0900

    get매서드의 활용

--- databaseM/READ.js
+++ databaseM/READ.js
@@ -2,13 +2,20 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('../database/info.db');
 
-db.all("SELECT * FROM info", (err,rows)=>{
-  if(err){
-    console.log(err.message);
-  }
-  rows.forEach((row)=>{
-    console.log(`${row.user}-${row.age}`);
+function getUserInfo(user){
+  const sql = 'SELECT user, age from info WHERE user =?';
+
+  db.get(sql,[user],(err,row)=>{
+    if(err){
+      return console.error(err.message);
+    } else if (row) {
+      console.log(`찾았습니다. ${row.user}의 나이는 ${row.age}입니다.`);
+    } else {
+      console.log('can not found');
+    }
   });
-});
+}
+
+getUserInfo('Integer');
 
 db.close();
