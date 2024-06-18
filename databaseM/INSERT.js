613e0697e07771374005d3eb0fb096dfc1510c6a
KimInteger <kjs20151240@gmail.com>
2024-06-18 15:27:20 +0900

    data INSERT SONG

--- databaseM/INSERT.js
+++ databaseM/INSERT.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('../database/info.db');
 
-db.run("INSERT INTO info (user, age) VALUES(?,?)",["Integer",29],(err)=>{
+db.run("INSERT INTO info (user, age) VALUES(?,?)",["Song",31],(err)=>{
   if(err){
     console.log(err.message);
   }
