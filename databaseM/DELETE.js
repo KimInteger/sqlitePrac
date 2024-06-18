7e02f37e34d89864193f9025136883ee5e0f27af
KimInteger <kjs20151240@gmail.com>
2024-06-18 15:47:55 +0900

    DB 삭제기능 구현

--- databaseM/DELETE.js
+++ databaseM/DELETE.js
@@ -1,3 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('../database/info.db');
+
+db.run("DELETE FROM info WHERE user = ?",["Integer"],(err)=>{
+  if(err){
+    return console.error(err.message);
+  }
+  console.log('data deleted');
+});
+
+db.close();
