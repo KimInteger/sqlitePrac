dce3d6ba150934539b454816012c951e67c035cc
KimInteger <kjs20151240@gmail.com>
2024-06-18 15:43:27 +0900

    Integer의 age값을 수정함

--- databaseM/UPDATE.js
+++ databaseM/UPDATE.js
@@ -2,3 +2,10 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('../database/info.db');
 
+db.run("UPDATE info SET age = ? WHERE user =?",[27,"Integer"],(err)=>{
+  if(err){
+    return console.error(err.message);
+  }
+
+  console.log("complete updated");
+});
