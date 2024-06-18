a2decd75c9ae214569563c15b8203c0a0375cb9b
KimInteger <kjs20151240@gmail.com>
2024-06-18 15:19:56 +0900

    infoTable생성

--- databaseM/CREATE.js
+++ databaseM/CREATE.js
@@ -1,3 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('../database/info.db');
+
+db.run("CREATE TABLE IF NOT EXISTS info (user TEXT, age INTEGER)",(err)=>{
+  if(err){
+    console.log(err.message);
+  }
+  console.log('table created');
+});
